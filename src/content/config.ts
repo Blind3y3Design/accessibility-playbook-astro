4b2bc1ab495c9c9b00a7a98c43ccc7f73440675c
Adam Sedwick <asedwick@blind3y3design.com>
2023-12-12 02:58:21 -0600

    initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,30 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+const itemSchema = z.object({
+  additionalResources: z
+    .array(
+      z.object({
+        title: z.string(),
+        link: z.string(),
+      }),
+    )
+    .optional(),
+  index: z.number().optional(),
+  subTitle: z.string(),
+  title: z.string(),
+  tags: z.array(z.string()),
+});
+
+export type ItemSchema = z.infer<typeof itemSchema>;
+
+// Define a `type` and `schema` for each collection
+const basicsCollection = defineCollection({
+  type: "content",
+  schema: itemSchema,
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  basics: basicsCollection,
+};
